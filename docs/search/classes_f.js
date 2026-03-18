f52be0f2be67bc965b9ae0f7bd7a74e43b688b09
maybetree <maybetree@stratal.systems>
2026-03-18 15:31:37 +0100

    Clean up GenerateDocs.py
    - Refactor into multiple functions
    - Use newer pathlib+subprocess in place of os module
    - Fail generatedocs.py early if missing dependencies
    - Add docs output files to gitignore
    - Add mypy, ruff config (currently only for GenerateDocs.py file)

--- docs/search/classes_f.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['vecgpodict_0',['vecgpodict',['../classPyPO_1_1Structs_1_1vecGPODict.html',1,'PyPO.Structs.vecGPODict'],['../structvecGPODict.html',1,'vecGPODict']]],
-  ['vecgpodictf_1',['vecgpodictf',['../classPyPO_1_1Structs_1_1vecGPODictf.html',1,'PyPO.Structs.vecGPODictf'],['../structvecGPODictf.html',1,'vecGPODictf']]],
-  ['vectorinput_2',['VectorInput',['../classGUI_1_1ParameterForms_1_1simpleInputWidgets_1_1VectorInput.html',1,'GUI::ParameterForms::simpleInputWidgets']]]
-];
